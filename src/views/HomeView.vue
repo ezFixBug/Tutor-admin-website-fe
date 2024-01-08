9cfcaef977601900e40b1af4386ffee335813042
lyhuynhthi <lyhuynhthi@ncc.asia>
2024-01-08 22:17:18 +0700

    payment chart

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,7 +7,8 @@ import {
   mdiBookAccount,
   mdiCommentArrowLeft,
   mdiNoteEdit,
-  mdiCommentQuote
+  mdiCommentQuote,
+  mdiReload
 } from '@mdi/js'
 import * as chartConfig from '@/components/Charts/chart.config.js'
 import LineChart from '@/components/Charts/LineChart.vue'
@@ -25,11 +26,83 @@ import SectionBannerStarOnGitHub from '@/components/SectionBannerStarOnGitHub.vu
 import $http from '@/services/httpService'
 import get from 'lodash/get'
 
-const chartData = ref(null)
-
-const fillChartData = () => {
-  chartData.value = chartConfig.sampleChartData()
-}
+const fetchDataChart = [
+  {
+    sum_price: '8720',
+    total_price_including_tax: 11760,
+    total_price_excluding_tax: 9800,
+    net_price_including_tax: 11760,
+    net_price_excluding_tax: 9800,
+    customer_count: 2,
+    value: 5880,
+    label: '2023-44'
+  },
+  {
+    sum_price: '1800',
+    total_price_including_tax: 1800,
+    total_price_excluding_tax: 1500,
+    net_price_including_tax: 1800,
+    net_price_excluding_tax: 1500,
+    customer_count: 1,
+    value: 1800,
+    label: '2023-45'
+  },
+  {
+    label: '2023-46',
+    value: 0,
+    customer_count: 0
+  },
+  {
+    label: '2023-47',
+    value: 0,
+    customer_count: 0
+  },
+  {
+    sum_price: '3740',
+    total_price_including_tax: 3740,
+    total_price_excluding_tax: 3163,
+    net_price_including_tax: 3740,
+    net_price_excluding_tax: 3163,
+    customer_count: 1,
+    value: 3740,
+    label: '2023-48'
+  },
+  {
+    label: '2023-49',
+    value: 0,
+    customer_count: 0
+  },
+  {
+    label: '2023-50',
+    value: 0,
+    customer_count: 0
+  },
+  {
+    label: '2023-51',
+    value: 0,
+    customer_count: 0
+  },
+  {
+    sum_price: '13502',
+    total_price_including_tax: 7000,
+    total_price_excluding_tax: 5926,
+    net_price_including_tax: 7000,
+    net_price_excluding_tax: 5926,
+    customer_count: 0,
+    value: null,
+    label: '2023-52'
+  },
+  {
+    label: '2024-01',
+    value: 0,
+    customer_count: 0
+  },
+  {
+    label: '2024-02',
+    value: 0,
+    customer_count: 0
+  }
+]
 
 const is_loading = ref(false)
 const statistics = ref({})
@@ -37,7 +110,7 @@ onMounted(async () => {
   is_loading.value = true
   const res = await $http.get('/admin/statistics')
   if (get(res, 'data.result', false)) {
-    statistics.value = res.data.statistics;
+    statistics.value = res.data.statistics
   }
   is_loading.value = false
 })
@@ -47,6 +120,15 @@ const mainStore = useMainStore()
 const clientBarItems = computed(() => mainStore.clients.slice(0, 4))
 
 const transactionBarItems = computed(() => mainStore.history)
+
+const chartData = {
+  labels: fetchDataChart.map((item) => item.label),
+  datasets: [
+    {
+      data:  fetchDataChart.map((item) => item.value || 0)
+    }
+  ]
+}
 </script>
 
 <template>
@@ -66,14 +148,24 @@ const transactionBarItems = computed(() => mainStore.history)
           :number="statistics.total_student"
           label="Học sinh"
         />
-        <CardBoxWidget color="text-red-500" :icon="mdiBookAccount" :number="statistics.total_course" label="Khóa học" />
+        <CardBoxWidget
+          color="text-red-500"
+          :icon="mdiBookAccount"
+          :number="statistics.total_course"
+          label="Khóa học"
+        />
         <CardBoxWidget
           color="text-yellow-500"
           :icon="mdiCommentArrowLeft"
           :number="statistics.total_request"
           label="Yêu cầu gia sư"
         />
-        <CardBoxWidget color="text-slate-500" :icon="mdiNoteEdit" :number="statistics.total_post" label="Bài viết" />
+        <CardBoxWidget
+          color="text-slate-500"
+          :icon="mdiNoteEdit"
+          :number="statistics.total_post"
+          label="Bài viết"
+        />
         <CardBoxWidget
           color="text-black-500"
           :icon="mdiCommentQuote"
@@ -107,9 +199,9 @@ const transactionBarItems = computed(() => mainStore.history)
         </div>
       </div>
 
-      <!-- <SectionBannerStarOnGitHub class="mt-6 mb-6" />
+      <SectionBannerStarOnGitHub class="mt-6 mb-6" />
 
-      <SectionTitleLineWithButton :icon="mdiChartPie" title="Trends overview">
+      <SectionTitleLineWithButton :icon="mdiChartPie" title="Thống kê giao dịch">
         <BaseButton :icon="mdiReload" color="whiteDark" @click="fillChartData" />
       </SectionTitleLineWithButton>
 
@@ -117,7 +209,7 @@ const transactionBarItems = computed(() => mainStore.history)
         <div v-if="chartData">
           <line-chart :data="chartData" class="h-96" />
         </div>
-      </CardBox> -->
+      </CardBox>
     </SectionMain>
   </LayoutAuthenticated>
 </template>
