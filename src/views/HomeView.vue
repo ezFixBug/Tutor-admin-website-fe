a8df3be2d557809016ebf46da085a6f02c393df9
lyhuynhthi <lyhuynhthi@ncc.asia>
2024-01-10 20:39:55 +0700

    payment

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { computed, ref, onMounted } from 'vue'
+import { computed, ref, onMounted, reactive } from 'vue'
 import { useMainStore } from '@/stores/main'
 import {
   mdiAccountMultiple,
@@ -7,17 +7,12 @@ import {
   mdiBookAccount,
   mdiCommentArrowLeft,
   mdiNoteEdit,
-  mdiCommentQuote,
-  mdiReload
+  mdiCommentQuote
 } from '@mdi/js'
-import * as chartConfig from '@/components/Charts/chart.config.js'
 import LineChart from '@/components/Charts/LineChart.vue'
 import SectionMain from '@/components/SectionMain.vue'
 import CardBoxWidget from '@/components/CardBoxWidget.vue'
 import CardBox from '@/components/CardBox.vue'
-import TableSampleClients from '@/components/TableSampleClients.vue'
-import NotificationBar from '@/components/NotificationBar.vue'
-import BaseButton from '@/components/BaseButton.vue'
 import CardBoxTransaction from '@/components/CardBoxTransaction.vue'
 import CardBoxClient from '@/components/CardBoxClient.vue'
 import LayoutAuthenticated from '@/layouts/LayoutAuthenticated.vue'
@@ -26,94 +21,31 @@ import SectionBannerStarOnGitHub from '@/components/SectionBannerStarOnGitHub.vu
 import $http from '@/services/httpService'
 import get from 'lodash/get'
 
-const fetchDataChart = [
-  {
-    sum_price: '8720',
-    total_price_including_tax: 11760,
-    total_price_excluding_tax: 9800,
-    net_price_including_tax: 11760,
-    net_price_excluding_tax: 9800,
-    customer_count: 2,
-    value: 5880,
-    label: '2023-44'
-  },
-  {
-    sum_price: '1800',
-    total_price_including_tax: 1800,
-    total_price_excluding_tax: 1500,
-    net_price_including_tax: 1800,
-    net_price_excluding_tax: 1500,
-    customer_count: 1,
-    value: 1800,
-    label: '2023-45'
-  },
-  {
-    label: '2023-46',
-    value: 0,
-    customer_count: 0
-  },
-  {
-    label: '2023-47',
-    value: 0,
-    customer_count: 0
-  },
-  {
-    sum_price: '3740',
-    total_price_including_tax: 3740,
-    total_price_excluding_tax: 3163,
-    net_price_including_tax: 3740,
-    net_price_excluding_tax: 3163,
-    customer_count: 1,
-    value: 3740,
-    label: '2023-48'
-  },
-  {
-    label: '2023-49',
-    value: 0,
-    customer_count: 0
-  },
-  {
-    label: '2023-50',
-    value: 0,
-    customer_count: 0
-  },
-  {
-    label: '2023-51',
-    value: 0,
-    customer_count: 0
-  },
-  {
-    sum_price: '13502',
-    total_price_including_tax: 7000,
-    total_price_excluding_tax: 5926,
-    net_price_including_tax: 7000,
-    net_price_excluding_tax: 5926,
-    customer_count: 0,
-    value: null,
-    label: '2023-52'
-  },
-  {
-    label: '2024-01',
-    value: 0,
-    customer_count: 0
-  },
-  {
-    label: '2024-02',
-    value: 0,
-    customer_count: 0
-  }
-]
-
 const is_loading = ref(false)
 const statistics = ref({})
 onMounted(async () => {
   is_loading.value = true
-  const res = await $http.get('/admin/statistics')
-  if (get(res, 'data.result', false)) {
-    statistics.value = res.data.statistics
-  }
+  getStatistics()
+  console.log(statistics.value)
   is_loading.value = false
 })
+
+const getStatistics = async () => {
+  const res = await $http.get('/admin/statistics', {
+    ...filter
+  })
+  if (get(res, 'data.result', false)) {
+    statistics.value = res.data.statistics
+    PaymentsCourseChartData.value = {
+      labels: statistics.value.payments_course?.map((item) => item.label),
+      datasets: [
+        {
+          data: statistics.value.payments_course?.map((item) => item.value || 0)
+        }
+      ]
+    }
+  }
+}
 
 const mainStore = useMainStore()
 
@@ -121,20 +53,50 @@ const clientBarItems = computed(() => mainStore.clients.slice(0, 4))
 
 const transactionBarItems = computed(() => mainStore.history)
 
-const chartData = {
-  labels: fetchDataChart.map((item) => item.label),
-  datasets: [
-    {
-      data:  fetchDataChart.map((item) => item.value || 0)
-    }
-  ]
+const PaymentsCourseChartData = ref({
+  labels: [],
+  datasets: []
+})
+
+const handleFilter = () => {
+  getStatistics()
+  console.log(12312);
 }
+
+const filterYear = [2024, 2023, 2022, 2021]
+
+const filterMonth = [null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+const filter = reactive({
+  year: filterYear[0],
+  month: null
+})
 </script>
 
 <template>
   <spinner :is_loading="is_loading" />
   <LayoutAuthenticated>
     <SectionMain>
+      <SectionTitleLineWithButton :icon="mdiChartPie" title="Thống kê giao dịch">
+        <div class="flex space-x-2">
+          <div class="space-x-2">
+            <label for="filter_year">Năm</label>
+            <select id="filter_year" v-model="filter.year" @change="handleFilter">
+              <option v-for="year in filterYear" :key="year" :value="year">{{ year }}</option>
+            </select>
+          </div>
+          <div class="space-x-2">
+            <label for="filter_month">Tháng</label>
+            <select id="filter_month" v-model="filter.month" @change="handleFilter">
+              <option v-for="month in filterMonth" :key="month" :value="month">{{ month }}</option>
+            </select>
+          </div>
+        </div>
+      </SectionTitleLineWithButton>
+
+      <CardBox class="mb-6">
+        <line-chart :data="PaymentsCourseChartData" class="h-96" />
+      </CardBox>
       <div class="grid grid-cols-1 gap-6 lg:grid-cols-3 mb-6">
         <CardBoxWidget
           color="text-emerald-500"
@@ -200,16 +162,6 @@ const chartData = {
       </div>
 
       <SectionBannerStarOnGitHub class="mt-6 mb-6" />
-
-      <SectionTitleLineWithButton :icon="mdiChartPie" title="Thống kê giao dịch">
-        <BaseButton :icon="mdiReload" color="whiteDark" @click="fillChartData" />
-      </SectionTitleLineWithButton>
-
-      <CardBox class="mb-6">
-        <div v-if="chartData">
-          <line-chart :data="chartData" class="h-96" />
-        </div>
-      </CardBox>
     </SectionMain>
   </LayoutAuthenticated>
 </template>
